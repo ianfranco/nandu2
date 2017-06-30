d070329ed48fc361e64b27f21da3a9f2d900a2bb
ianfranco <ianfranco@WIN7>
2017-06-29 20:29:56 -0400

    Vena Boletos

--- src/main/java/com/areatecnica/sigf/dao/impl/IVentaBoletoDaoImpl.java
+++ src/main/java/com/areatecnica/sigf/dao/impl/IVentaBoletoDaoImpl.java
@@ -57,4 +57,9 @@ public class IVentaBoletoDaoImpl extends GenericDAOImpl<VentaBoleto> implements 
         }
     }
 
+    @Override
+    public List<VentaBoleto> findByProceso() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
